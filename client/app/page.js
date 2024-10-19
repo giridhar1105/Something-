3df2ed2e9f765dfca8d143157ab0533e0705ced9
Giridhar <giridhaar1105@gmail.com>
2024-10-19 08:22:54 +0530

    Side Scroll Done

--- client/app/page.js
+++ client/app/page.js
@@ -6,9 +6,7 @@ import "./globals.css";
 export default function LifeIsBeautiful () {
   return (
     <div>
-         <title>Gallery</title>
-         <link href="https://fonts.googleapis.com/css?family=Signika+Negative&display=swap" rel="stylesheet" />
-         <Sidescroll />
-    </div>
+      <Sidescroll />
+  </div>
   )
 }

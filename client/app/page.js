c52f24f1fcae1c08befc6eff970a2f5e4ab4d1af
Giridhar <giridhaar1105@gmail.com>
2024-10-19 11:02:49 +0530

    Scate

--- client/app/page.js
+++ client/app/page.js
@@ -2,12 +2,13 @@ import Airpods from "./Airpods/page";
 import TextScroll from "./TextScroll/page";
 import Sidescroll from "./Sidescroll/page";
 import Firstpage from "./Firstpage/page";
+import Scate from "./Scate/page";
 import "./globals.css";
 
 export default function LifeIsBeautiful () {
   return (
     <div>
-      <Firstpage />
+      <Scate />
     </div>
   )
 }

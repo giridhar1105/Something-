b4cbcf42e59157ba7ec2f5dcbe62d277d11c3866
Giridhar <135593477+giridhar1105@users.noreply.github.com>
2025-02-15 22:28:34 +0530

    Update page.js

--- client/app/page.js
+++ client/app/page.js
@@ -1,18 +1,9 @@
 import Airpods from "./Airpods/page";
-import TextScroll from "./TextScroll/page";
-import Sidescroll from "./Sidescroll/page";
-import Firstpage from "./Firstpage/page";
-import Scate from "./Scate/page";
-import "./globals.css";
 
 export default function LifeIsBeautiful () {
   return (
     <div>
       <Airpods />
-      <TextScroll />
-      <Sidescroll />
-      <Firstpage />
-      <Scate />
     </div>
   )
 }
